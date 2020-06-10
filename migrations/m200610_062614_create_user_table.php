bb7b6c77c2f36efd4aa4e11e05d20ab49a025aa6
NyanFisher <zaikinmaxv@gmail.com>
2020-06-10 11:38:32 +0500

    инициализация проекта

---
+++ migrations/m200610_062614_create_user_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%user}}`.
+ */
+class m200610_062614_create_user_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%user}}', [
+            'id' => $this->primaryKey(),
+            'username' => $this->string(32),
+            'password' => $this->string(255),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%user}}');
+    }
+}
---
+++ migrations/m200610_063339_create_reservation_table.php
@@ -0,0 +1,63 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%reservation}}`.
+ * Has foreign keys to the tables:
+ *
+ * - `{{%user}}`
+ */
+class m200610_063339_create_reservation_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%reservation}}', [
+            'id' => $this->primaryKey(),
+            'datatime_created' => $this->notNull()->datetime(),
+            'data_arrival' => $this->date(),
+            'status' => $this->boolean()->defaultValue(0),
+            'user_id' => $this->integer(),
+        ]);
+
+        // creates index for column `user_id`
+        $this->createIndex(
+            '{{%idx-reservation-user_id}}',
+            '{{%reservation}}',
+            'user_id'
+        );
+
+        // add foreign key for table `{{%user}}`
+        $this->addForeignKey(
+            '{{%fk-reservation-user_id}}',
+            '{{%reservation}}',
+            'user_id',
+            '{{%user}}',
+            'id',
+            'CASCADE'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        // drops foreign key for table `{{%user}}`
+        $this->dropForeignKey(
+            '{{%fk-reservation-user_id}}',
+            '{{%reservation}}'
+        );
+
+        // drops index for column `user_id`
+        $this->dropIndex(
+            '{{%idx-reservation-user_id}}',
+            '{{%reservation}}'
+        );
+
+        $this->dropTable('{{%reservation}}');
+    }
+}
